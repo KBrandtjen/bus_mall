079b3ae0999c53e765ed70933cb652eedb187924
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-08-01 15:29:02 -0700

    updated folders and initial js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,33 @@
-'use strict'
+'use strict';
+
+var imageArray = [];
+
+function picObject (picName, filePath) {
+  this.picName = picName;
+  this.filePath = filePath;
+  this.tallyClicked = 0;
+  this.tallyDisplayed = 0;
+  imageArray.push(this);
+}
+
+new picObject('Baby Broom', 'img/sweep.jpg')
+new picObject('Octopus Tail USB', 'img/usb.jpg')
+new picObject('Wine Glass', 'img/wine_glass.jpg')
+new picObject('Watering Can', 'img/water_can.jpg')
+new picObject('Tauntaun', 'img/tauntaun.jpg')
+new picObject('Sleeping Shark Bag', 'img/shark.jpg')
+new picObject('Pizza Scissors', 'img/scissors.jpg')
+new picObject('Pet Sweeper', 'img/pet_sweep.jpg')
+new picObject('Pen Cap Utensils', 'img/pen.jpg')
+new picObject('Dragon Meat', 'img/dragon.jpg')
+new picObject('Dog Beak', 'img/dog_duck.jpg')
+new picObject('Cthulhu', 'img/cthulhu.jpg')
+new picObject('Chair', 'img/chair.jpg')
+new picObject('Meatball Bubblegum', 'img/bubblegum.jpg')
+new picObject('Breakfast Maker', 'img/breakfast.jpg')
+new picObject('Open-Toe Boots', 'img/boots.jpg')
+new picObject('Bathroom Caddy', 'img/bathroom.jpg')
+new picObject('Banana Slicer', 'img/banana.jpg')
+new picObject('R2D2 Bag', 'img/bag.jpg')
+
+console.log(picObject);
--- user_stories.md
+++ user_stories.md
@@ -1,1 +1,15 @@
-# Bus Mall User Stories
+# BusMall User Stories
+
+
+As a developer, I want to break down the test into many small steps so that I can account for all of the variables within the game
+
+As a developer, I want to analyze each small step of the game to get rid of inconsistencies and make the game as simple as possible. 
+
+
+As a marketer, I want the game to be consistent no matter who the user is so that each time it's ran we are collecting the same data and not worrying about inconsistencies.
+
+
+As a marketer, I want the results of the test to be displayed in a clear and concise format so that we can easily analyze the data
+
+
+As a user, I want the test to be easy to work through so that I can focus on the pictures not on how to work the test.
