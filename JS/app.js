41186ca423b87e4d35f21998435d393d4d6f502a
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-08-02 18:07:12 -0700

    added chart to page, fully functional

--- JS/app.js
+++ JS/app.js
@@ -2,8 +2,11 @@
 
 var imageArray = [];
 var totalClicks = 0;
+var picNameArray = [];
+var clickArray = [];
+var busMallChart;
 
-function picObject (picName, filePath) {
+function PicObject (picName, filePath) {
   this.picName = picName;
   this.filePath = filePath;
   this.clicks = 0;
@@ -11,26 +14,26 @@ function picObject (picName, filePath) {
   imageArray.push(this);
 }
 
-new picObject('Baby Broom', 'img/sweep.jpg');//eslint-disable-line
-new picObject('Octopus Tail USB', 'img/usb.jpg');//eslint-disable-line
-new picObject('Wine Glass', 'img/wine_glass.jpg');//eslint-disable-line
-new picObject('Watering Can', 'img/water_can.jpg');//eslint-disable-line
-new picObject('Tauntaun', 'img/tauntaun.jpg');//eslint-disable-line
-new picObject('Sleeping Shark Bag', 'img/shark.jpg');//eslint-disable-line
-new picObject('Pizza Scissors', 'img/scissors.jpg');//eslint-disable-line
-new picObject('Pet Sweeper', 'img/pet_sweep.jpg');//eslint-disable-line
-new picObject('Pen Cap Utensils', 'img/pen.jpg');//eslint-disable-line
-new picObject('Dragon Meat', 'img/dragon.jpg');//eslint-disable-line
-new picObject('Dog Beak', 'img/dog_duck.jpg');//eslint-disable-line
-new picObject('Cthulhu', 'img/cthulhu.jpg');//eslint-disable-line
-new picObject('Chair', 'img/chair.jpg');//eslint-disable-line
-new picObject('Meatball Bubblegum', 'img/bubblegum.jpg');//eslint-disable-line
-new picObject('Breakfast Maker', 'img/breakfast.jpg');//eslint-disable-line
-new picObject('Open-Toe Boots', 'img/boots.jpg');//eslint-disable-line
-new picObject('Bathroom Caddy', 'img/bathroom.jpg');//eslint-disable-line
-new picObject('Banana Slicer', 'img/banana.jpg');//eslint-disable-line
-new picObject('R2D2 Bag', 'img/bag.jpg');//eslint-disable-line
-new picObject('Unicorn Meat', 'img/unicorn.jpg');//eslint-disable-line
+new PicObject('Baby Broom', 'img/sweep.jpg');//eslint-disable-line
+new PicObject('Octopus Tail USB', 'img/usb.jpg');//eslint-disable-line
+new PicObject('Wine Glass', 'img/wine_glass.jpg');//eslint-disable-line
+new PicObject('Watering Can', 'img/water_can.jpg');//eslint-disable-line
+new PicObject('Tauntaun', 'img/tauntaun.jpg');//eslint-disable-line
+new PicObject('Sleeping Shark Bag', 'img/shark.jpg');//eslint-disable-line
+new PicObject('Pizza Scissors', 'img/scissors.jpg');//eslint-disable-line
+new PicObject('Pet Sweeper', 'img/pet_sweep.jpg');//eslint-disable-line
+new PicObject('Pen Cap Utensils', 'img/pen.jpg');//eslint-disable-line
+new PicObject('Dragon Meat', 'img/dragon.jpg');//eslint-disable-line
+new PicObject('Dog Beak', 'img/dog_duck.jpg');//eslint-disable-line
+new PicObject('Cthulhu', 'img/cthulhu.jpg');//eslint-disable-line
+new PicObject('Chair', 'img/chair.jpg');//eslint-disable-line
+new PicObject('Meatball Bubblegum', 'img/bubblegum.jpg');//eslint-disable-line
+new PicObject('Breakfast Maker', 'img/breakfast.jpg');//eslint-disable-line
+new PicObject('Open-Toe Boots', 'img/boots.jpg');//eslint-disable-line
+new PicObject('Bathroom Caddy', 'img/bathroom.jpg');//eslint-disable-line
+new PicObject('Banana Slicer', 'img/banana.jpg');//eslint-disable-line
+new PicObject('R2D2 Bag', 'img/bag.jpg');//eslint-disable-line
+new PicObject('Unicorn Meat', 'img/unicorn.jpg');//eslint-disable-line
 
 var randomNumberArray = [];
 var previouslyShown = [];
@@ -113,6 +116,10 @@ function handleClick(event) {
     photoSection.removeEventListener('click', handleClick);
     console.log('max number of clicks reached');
     resultsButton.hidden = false;
+    for (var j = 0; j < imageArray.length; j++) {
+      picNameArray.push(imageArray[j].picName);
+      clickArray.push(imageArray[j].clicks);
+    }
     return;
   }
 
@@ -121,7 +128,7 @@ function handleClick(event) {
 }
 
 function handleResultsButton() {
-  alert ('this is where you draw the chart');
+  drawChart();
 }
 
 //exectuing code below
@@ -133,46 +140,32 @@ photoSection.addEventListener('click', handleClick);
 var resultsButton = document.getElementById('resultsButton');
 resultsButton.addEventListener('click', handleResultsButton);
 
+var data = {
+  labels: picNameArray, // titles array we declared earlier
+  datasets: [
+    {
+      data: clickArray, // votes array we declared earlier
+      backgroundColor: [
+        'bisque',
+        'darkgray',
+        'burlywood',
+        'lightblue',
+        'navy'
+      ],
+      hoverBackgroundColor: [
+        'purple',
+        'purple',
+        'purple',
+        'purple',
+        'purple'
+      ]
+    }]
+};
 
-// var getLeftPhoto = document.getElementById('leftPhoto');
-// var getCenterPhoto = document.getElementById('centerPhoto');
-// var getRightPhoto = document.getElementById('rightPhoto');
-// var clickSection = document.getElementById('photoSection');
-//
-// function randomNumberGenerator () {
-//   var randLeftNumber = Math.floor(Math.random() * imageArray.length);
-//   console.log(randLeftNumber);
-//   var randCenterNumber = Math.floor(Math.random() * imageArray.length);
-//   console.log(randCenterNumber);
-//   var randRightNumber = Math.floor(Math.random() * imageArray.length);
-//   console.log(randRightNumber);
-// };
-//
-// randomNumberGenerator();
-//
-// randomNumberArray.push(randLeftNumber, randCenterNumber, randRightNumber);
-//
-//
-// getLeftPhoto.src = imageArray[randomNumberArray[0]].filePath;
-// getCenterPhoto.src = imageArray[randonNumberArray[1]].filePath;
-// getRightPhoto.src = imageArray[randonNumberArray[2]].filePath;
-//
-// randomNumberArray.push(randomNumber, randomNumber1, randomNumber2);
-
-
-
-//While for previously shown
-
-// while (randomNumberArray[0] === previouslyShown[0] ||
-//       randomNumberArray[0] === previouslyShown[1] ||
-//       randomNumberArray[0] === previouslyShown[2] ||
-//       randomNumberArray[1] === previouslyShown[0] ||
-//       randomNumberArray[1] === previouslyShown[1] ||
-//       randomNumberArray[1] === previouslyShown[2] ||
-//       randomNumberArray[2] === previouslyShown[0] ||
-//       randomNumberArray[2] === previouslyShown[1] ||
-//       randomNumberArray[2] === previouslyShown[2]) {
-//   randomNumberArray[0] = (Math.floor(Math.random() * imageArray.length));
-//   randomNumberArray[1] = (Math.floor(Math.random() * imageArray.length));
-//   randomNumberArray[2] = (Math.floor(Math.random() * imageArray.length));
-// }
+function drawChart() {
+  var ctx = document.getElementById('chart').getContext('2d');
+  busMallChart = new Chart(ctx,{
+    type: 'bar',
+    data: data,
+  });
+};
